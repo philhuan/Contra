d08a17c52b5d57069e3c79e37a0d9d774d080bae
wei zheng <tobe.zheng.wei@gmail.com>
2018-05-20 17:00:04 +1000

    comment for diving fix

--- Game/Bill.cpp
+++ Game/Bill.cpp
@@ -454,6 +454,7 @@ void Bill::physical()
 
 	if (position.getY() > 375&&position.getX()+gameActivity->backGround->curPosition->getX()<4500)
 		act = INWATER;
+	//this will toggle the isDive property
 	if (act == INWATER && directionY == DOWN)
 		isDive = true;
 	else
